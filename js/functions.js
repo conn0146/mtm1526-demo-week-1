d1d06fc98d48a37bfa2b6c87e34ceb5cc27b759e
conn0146 <connell.connect@gmail.com>
2012-01-13 12:28:23 -0500

    Added more code

--- js/functions.js
+++ js/functions.js
@@ -7,7 +7,11 @@ Javascript multi-line comment
 alert('Warning! You are a douchebag');
 
 // 'prompt()' is for user input, has a text field for the user to enter some text
-var name = prompt('What is your name?');
+var name;
+
+while (!name) {
+	name = prompt('What is your name?');
+}
 
 // 'console.log()' writes out the content of a variable for debugging
 // identical to ActionScript's 'trace()'
@@ -16,3 +20,28 @@ console.log(name);
 // 'document.write()' allows us to output stuff to the end of the HTML file
 // the '+' operator is for combining strings together
 document.write('Hello, '+name);
+
+for (var i=0; i<5; i++) {
+	document.write('<br>' + i);
+	document.write('<h1>' + i + '</h1>');
+}
+
+function isNameCool () {
+	var name = prompt('What is your name?');
+	
+	if (name == 'Jason') {
+		document.write('That is an awesome name!');
+	} else {
+		document.write("I don't like your name");
+	}
+}
+
+isNameCool();
+
+var writeNameMultipleTimes = function () {
+	for (var i = 0; i < 5; i++ ) {
+		document.write('<strong>Jason</strong><br>');
+	}
+};
+
+writeNameMultipleTimes();
